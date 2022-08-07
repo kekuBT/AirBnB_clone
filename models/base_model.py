3cd7a30d1b7fd874567cbbb48de393d586b0a846
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-08-07 11:26:20 +0300

    edit

--- models/base_model.py
+++ models/base_model.py
@@ -2,44 +2,71 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """Base Model Class
+    This is the Base Model that take care of the
+    initialization, serialization and deserialization
+    of the future instances.
+    Attributes:
+        id (str): It's an UUID for when an instance is created.
+        created_at (datetime): The current date and time that
+            an instance is created.
+        updated_at (datetime): The current date and time that
+            an instance is created and it will be updated every
+            time that the object changes.
+    """
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        """Base Model __init__ Method
+        Here, the default values of a Base Model
+        instance are initialized.
+        """
+        if kwargs:
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Representation of the class for the user
+        Example:
+            $ bm = BaseModel()
+            $ print(bm)
+            This method prints the content of the Base Model
+            class with this format
+            $ [<class name>] (<self.id>) <self.__dict__>
+        """
+        return '[{0}] ({1}) {2}'.format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """Updates a Base Model instance
+        Updates the public instance attribute `updated_at`
+        with the current datetime and dumps the class data
+        into a file
+        """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """Converts the information of the class to human-readable format
+        Returns a new dictionary containing all keys/values
+        of __dict__ of the instance.
+        """
+        class_info = self.__dict__.copy()
+        class_info['__class__'] = self.__class__.__name__
+        class_info['created_at'] = self.created_at.isoformat()
+        class_info['updated_at'] = self.updated_at.isoformat()
+
+        return class_info
