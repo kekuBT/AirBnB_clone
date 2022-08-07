4de70adf7aba404a394f5c1384054b242136be3f
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-08-07 11:13:47 +0300

    edit Docs

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,53 @@
 #!/usr/bin/python3
 
+
+""" Base module """
 import uuid
 from datetime import datetime
-# This imports the variable storage
+# import the variable storage
 import models
 
 
-class BaseModel():
-    """" defines all common attributes/methods for other classes """
+class BaseModel:
+    """ class for all other classes to inherit from """
 
     def __init__(self, *args, **kwargs):
         """ Constructor and re-create an instance with
         this dictionary representation"""
         if len(kwargs) > 0:
-            # each key to this dictionary is an attribute name
-            # each value is the value of this attribute name
+            # each key of this dictionary is an attribute name
+            # each value of this dictionary is the value of this attribute name
             for key, value in kwargs.items():
                 if key == "updated_at":
-                    # convert string date to datetime object
+                    # Convert string date to datetime object
                     # strptime (string parse time): Parse a string into a -
                     # datetime object given a corresponding format
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "created_at":
-                    value = datetime.strptime(value, "Y-%m-%dT%H:%M:%S.%f")
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "__class__":
-                    # this happens because __class__ is no mandatory in output
+                    # This happens because __class__ is not mandatory in output
                     continue
+
                 setattr(self, key, value)
         else:
-            # generate UUID
+            # Generate a random UUID
             self.id = str(uuid.uuid4())
-            # Assign new instances with current datetime
+            # assign with the current datetime when an instance is created
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # new instance add a call to the new(self) method
+            # if it’s a new instance add a call to the method new(self) on stge
             models.storage.new(self)
 
     def __str__(self):
-        """ overriding the __str__ method that returns a custom string object """
+        """ overriding the __str__ method that returns a custom
+        string object """
+        # Old-style: self.__class__.__name__
         class_name = type(self).__name__
         mssg = "[{0}] ({1}) {2}".format(class_name, self.id, self.__dict__)
-        return mssg
+        return (mssg)
 
+    # Public instance methods
     def save(self):
         """ updates the public instance attribute updated_at with
         the current datetime """
@@ -51,13 +57,15 @@ class BaseModel():
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance."""
-        # Define a dictionary and key __class__ that add to this dictionary with
-        # the class name of the object
+        # Define a dictionary and key __class__ that add to this dictionary
+        # with the class name of the object
         tdic = {}
         tdic["__class__"] = type(self).__name__
+        # loop over dict items and validate created_at and updated_at to
+        # convert in ISO format
         for n, i in self.__dict__.items():
             if isinstance(i, datetime):
                 tdic[n] = i.isoformat()
             else:
-                tdic[n] = 1
-        return tdic
+                tdic[n] = i
+        return (tdic)
