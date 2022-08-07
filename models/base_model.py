c3112165601f2efd814367acc0362cb74ea44136
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-08-07 11:22:34 +0300

    edit

--- models/base_model.py
+++ models/base_model.py
@@ -1,70 +1,45 @@
 #!/usr/bin/python3
-
-
-""" Base module """
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """ class for all other classes to inherit from """
+    """A base class for all hbnb models"""
 
     def __init__(self, *args, **kwargs):
-        """ Constructor and re-create an instance with
-        this dictionary representation"""
-        if len(kwargs) > 0:
-            # each key of this dictionary is an attribute name
-            # each value of this dictionary is the value of this attribute name
-            for key, value in kwargs.items():
-                if key == "updated_at":
-                    # Convert string date to datetime object
-                    # strptime (string parse time): Parse a string into a -
-                    # datetime object given a corresponding format
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "created_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "__class__":
-                    # This happens because __class__ is not mandatory in output
-                    continue
-
-                setattr(self, key, value)
-        else:
-            # Generate a random UUID
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
-            # assign with the current datetime when an instance is created
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # if it’s a new instance add a call to the method new(self) on stge
-            models.storage.new(self)
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """ overriding the __str__ method that returns a custom
-        string object """
-        # Old-style: self.__class__.__name__
-        class_name = type(self).__name__
-        mssg = "[{0}] ({1}) {2}".format(class_name, self.id, self.__dict__)
-        return mssg
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-    # Public instance methods
     def save(self):
-        """ updates the public instance attribute updated_at with
-        the current datetime """
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance."""
-        # Define a dictionary and key __class__ that add to this dictionary
-        # with the class name of the object
-        tdic = {}
-        tdic["__class__"] = type(self).__name__
-        # loop over dict items and validate created_at and updated_at to
-        # convert in ISO format
-        for n, i in self.__dict__.items():
-            if isinstance(i, datetime):
-                tdic[n] = i.isoformat()
-            else:
-                tdic[n] = i
-        return tdic
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
