216324bff7ec7ad81709ffd50c4519c531feb27a
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-08-12 10:17:52 +0300

    edit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ create a unique FileStorage instance for your application """
 
+from typing_extensions import Self
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage
-storage.reload()
+storage.reload(Self)
